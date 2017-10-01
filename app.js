2265626e6adcb0201163d3b5098f8204669f1dbd
Michael Davis <mdavis93@hotmail.com>
2017-10-01 02:53:40 -0500

    Assignment 7: COMPLETE

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ function onReady() {
     // Create a new input for the checkbox
     let checkbox = document.createElement('input');
 
+    // Create a delete button
+    let delBtn = document.createElement('button');
+    delBtn.innerText="Delete";
+
     // Set the input's type to checkbox
     checkbox.type = "checkbox";
 
@@ -27,9 +31,26 @@ function onReady() {
     // Attach the checkbox to the li
     newLi.appendChild(checkbox);
 
+    // Attach the delete button to the li
+    newLi.appendChild(delBtn);
+
     // Empty the input
     newToDoText.value = "";
   });
+
+  toDoList.addEventListener('click', (e) => {
+    if (e.target.innerText === "Delete") {
+      e.target.parentNode.parentNode.removeChild(
+        e.target.parentNode
+      );
+    }
+  });
+}
+
+function delToDo(e) {
+  // Target the UL
+  let liParent = e.target.parentNode.parentNode;
+  liParent.removeChild(e.target.parentNode);
 }
 
 window.onload = function() {
